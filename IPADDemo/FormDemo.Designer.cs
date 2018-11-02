27375ecfdadf224fcc4b76cd8d4d9d1be69d0b47
xuzeyu91 <286513187@qq.com>
2018-11-02 15:45:16 +0800

    picturebox
    picturebox

--- IPADDemo/FormDemo.Designer.cs
+++ IPADDemo/FormDemo.Designer.cs
@@ -52,6 +52,7 @@
             this.pictureBox1.Location = new System.Drawing.Point(13, 13);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(191, 185);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
